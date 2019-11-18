a87bbc75776d246516cde50f9c1bfad676133e8e
JackFogerson <40747086+JackFogerson@users.noreply.github.com>
2019-11-18 12:18:05 -0800

    Comments

--- src/com/PasswordManager/AccountsPaneItem.java
+++ src/com/PasswordManager/AccountsPaneItem.java
@@ -7,6 +7,7 @@ public class AccountsPaneItem extends JButton
 	private static final long serialVersionUID = 1L;
 	Account myAccount;
 	
+	//public constructor for AccountsPaneItem
 	public AccountsPaneItem(Account a)
 	{
 		myAccount = a;
