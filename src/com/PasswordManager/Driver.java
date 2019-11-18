bd368d37f4e3de185d3ecada976d854b1f2606ba
JackFogerson <40747086+JackFogerson@users.noreply.github.com>
2019-11-18 12:22:08 -0800

    Update Driver.java

--- src/com/PasswordManager/Driver.java
+++ src/com/PasswordManager/Driver.java
@@ -5,14 +5,18 @@ package com.PasswordManager;
  * @author	Nick Fulton, Jack Fogerson, Jack Gisel
  * @desc	This will handle everything needed to run JackPass.
  */
+
 public class Driver 
 {
 	public static void main(String[] args)
 	{
+		//Program defaults with logged out user
+		//prompts log-in screen and gets the account from user input
 		User loggedInUser = null;
 		LoginScreen ls = new LoginScreen();
 		loggedInUser = ls.getUser();
 		
+		//Creates new instance of JackPass and launches the program
 		PasswordManager pm = new PasswordManager(loggedInUser);
 		pm.launch();
 	}
