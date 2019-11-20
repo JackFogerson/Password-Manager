859bab36329a065cede13b95471bec6a49f08de2
JackFogerson <40747086+JackFogerson@users.noreply.github.com>
2019-11-19 17:05:53 -0800

    Account.java has functionality

--- src/com/PasswordManager/Account.java
+++ src/com/PasswordManager/Account.java
@@ -1,7 +1,38 @@
 package com.PasswordManager;
 
+
+/**
+ * @title	Account
+ * @author 	Nick Fulton, Jack Fogerson, Jack Gisel 
+ * @desc	Class to handle user's account info
+ */
 public class Account 
 {
+	//Variables for all accounts
+	String username;
+	String password;
+	String website;
+	
+	//Account constructor
+	public Account(String url, String u, String p) {
+		this.website = url;
+		this.username = u;
+		this.password = p;
+	}
+	
+	//Get methods for account
+	public String getURL() {
+		return this.website;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
 	public Account encrypt()
 	{
 		// TODO Make something that makes the accounts secure.
