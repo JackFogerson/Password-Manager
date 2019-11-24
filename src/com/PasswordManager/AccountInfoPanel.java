833560412a4a132dd40ee9ee48266a7bbe7cf280
JackFogerson <40747086+JackFogerson@users.noreply.github.com>
2019-11-24 14:53:40 -0800

    Added TODO statement to AccountInfoPanel

--- src/com/PasswordManager/AccountInfoPanel.java
+++ src/com/PasswordManager/AccountInfoPanel.java
@@ -2,6 +2,7 @@ package com.PasswordManager;
 
 import javax.swing.JPanel;
 
+//TODO: Add functionality to the class
 public class AccountInfoPanel extends JPanel
 {
 	private static final long serialVersionUID = 1L;
