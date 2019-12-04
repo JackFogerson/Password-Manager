3726423338262ad9dc5cd169a3a3e1a9b1772ede
Jack Gisel <jackrgisel@gmail.com>
2019-12-03 18:15:49 -0800

    encrpytion not working still

--- src/com/PasswordManager/Encryptor.java
+++ src/com/PasswordManager/Encryptor.java
@@ -1,56 +1,46 @@
 package com.PasswordManager;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 public class Encryptor {
-
-    //BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
-
     public Encryptor() {
-        //textEncryptor.setPassword("Jack");
-        return;
     }
-
-//    public String encryptPassword(String passwordToEncrypt) {
-//        String encryptedPassword = textEncryptor.encrypt(passwordToEncrypt);
-//        return encryptedPassword;
-//    }
-//
-//    public String decryptHash(String hashtoDecrpyt) {
-//        String decryptedPassword = textEncryptor.decrypt(hashtoDecrpyt);
-//        return decryptedPassword;
-//    }
 
     public static String encrypt(String strClearText,String strKey) throws Exception{
         String strData="";
 
-//        try {
-//            SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
-//            Cipher cipher=Cipher.getInstance("Blowfish");
-//            cipher.init(Cipher.ENCRYPT_MODE, skeyspec);
-//            byte[] encrypted=cipher.doFinal(strClearText.getBytes());
-//            strData=new String(encrypted);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            throw new Exception(e);
-//        }
-        return strClearText;
+        try {
+            SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
+            Cipher cipher=Cipher.getInstance("Blowfish");
+            cipher.init(Cipher.ENCRYPT_MODE, skeyspec);
+            byte[] encrypted=cipher.doFinal(strClearText.getBytes());
+            strData=new String(encrypted);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception(e);
+        }
+
+        return strData;
     }
 
     public static String decrypt(String strEncrypted,String strKey) throws Exception{
         String strData="";
 
-//        try {
-//            SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
-//            Cipher cipher=Cipher.getInstance("Blowfish");
-//            cipher.init(Cipher.DECRYPT_MODE, skeyspec);
-//            byte[] decrypted=cipher.doFinal(strEncrypted.getBytes());
-//            strData=new String(decrypted);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            throw new Exception(e);
-//        }
-        return strEncrypted;
+        try {
+            SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
+            Cipher cipher=Cipher.getInstance("Blowfish");
+            cipher.init(Cipher.DECRYPT_MODE, skeyspec);
+            byte[] decrypted=cipher.doFinal(strEncrypted.getBytes());
+            strData=new String(decrypted);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception(e);
+        }
+
+        return strData;
     }
 
 }
